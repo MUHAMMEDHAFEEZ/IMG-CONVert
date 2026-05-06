b257c58dc9933d38a49c0950e659d93c5900c710
MUHAMMEDHAFEEZ <m.hafeez@cairocamerarentals.com>
2026-05-06 19:09:45 +0300

    chore: update dependencies to explicit list and fix path phrasing in build script

--- build_windows.bat
+++ build_windows.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Installing dependencies...
-pip install -r requirements.txt
+pip install PyQt6 Pillow pyinstaller
 
 echo Building executable...
 python -m pyinstaller --onefile --windowed --name IMG-CONVert --icon=NONE image_converter.py
 
-echo Done! Executable is in the dist folder.
+echo Done! Executable is in dist folder.
 pause
