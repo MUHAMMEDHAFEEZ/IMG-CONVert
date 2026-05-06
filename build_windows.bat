20bd6b323f4abe325baedcc585c41676769d7393
MUHAMMEDHAFEEZ <m.hafeez@cairocamerarentals.com>
2026-05-06 19:06:35 +0300

    fix: update build script to execute pyinstaller as a python module

--- build_windows.bat
+++ build_windows.bat
@@ -3,7 +3,7 @@ echo Installing dependencies...
 pip install -r requirements.txt
 
 echo Building executable...
-pyinstaller --onefile --windowed --name IMG-CONVert --icon=NONE image_converter.py
+python -m pyinstaller --onefile --windowed --name IMG-CONVert --icon=NONE image_converter.py
 
 echo Done! Executable is in the dist folder.
 pause
